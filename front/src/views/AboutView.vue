0a2936f8b5324b9402c1a9f319b83dcd66029d90
unknown <dnpffldxjs12@gmail.com>
2023-05-16 17:10:24 +0900

    기본 설정, 통신만 되도록/ 일단 기본적으로 필요한 모듈들 설치

---
+++ front/src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+
+    <h1 @click="gettest">test</h1>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name:'TesT',
+  methods:{
+    gettest(){
+      axios({
+        method:'get',
+        url:'http://127.0.0.1:8000/',
+      })
+      .then((res)=>{
+        console.log(res);
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
